702c7e7d7bea390d9777f7e34ccccdec1caa2520
Shaikh Imran <skimran0306@gmail.com>
2024-04-16 01:48:33 +0530

    object key check

--- sum.test.js
+++ sum.test.js
@@ -1,35 +1,35 @@
 const sum = require('./sum');
-const data = require('./object');
-const falsy = require('./falsy');
-const _undefined = require('./_undefined');
-const checkObject = require('./_throwError');
-const fetchData = require('./_asyncFunc')
+// const data = require('./object');
+// const falsy = require('./falsy');
+// const _undefined = require('./_undefined');
+// const checkObject = require('./_throwError');
+// const fetchData = require('./_asyncFunc');
 
 test(' sum of two number', () => {
     expect(sum(1, 2)).toBe(3);
 })
 
-test('sum of 4 + 4', () => {
-    expect(4 + 4).toBe(8);
-})
+// test('sum of 4 + 4', () => {
+//     expect(4 + 4).toBe(8);
+// })
 
-test('object test value', () => {
-    expect(data).toEqual({ name: 'Jest testing', version: '29.7.0', description: 'Jest testing', file: 'object' });
-})
+// test('object test value', () => {
+//     expect(data).toEqual({ name: 'Jest testing', version: '29.7.0', description: 'Jest testing', file: 'object' });
+// })
 
-test('null falsy test', () => {
-    expect(falsy).toBeNull();
-})
+// test('null falsy test', () => {
+//     expect(falsy).toBeNull();
+// })
 
-test('undefined test', () => {
-    expect(_undefined).toBeUndefined();
-})
+// test('undefined test', () => {
+//     expect(_undefined).toBeUndefined();
+// })
 
-test('Throw new err', () => {
-    expect(() => {
-        checkObject('drink')
-    }).toThrow()
-})
+// test('Throw new err', () => {
+//     expect(() => {
+//         checkObject('drink')
+//     }).toThrow()
+// })
 
 // test('Async test', done => {
 //     function callback(data) {
@@ -44,6 +44,11 @@ test('Throw new err', () => {
 //     fetchData(callback);
 // })
 
-test('Async test with Promise', () => {
-    return expect(fetchData()).resolves.toBe('computer');
-});
+// test('Async test with Promise', () => {
+//     return expect(fetchData()).resolves.toBe('computer');
+// });
+
+
+// test('Promise test', () => {
+//     return expect(fetchData()).rejects.toThrow('error');
+// })
--- toBe/index.test.js
+++ toBe/index.test.js
@@ -6,6 +6,14 @@ const data = {
 describe('test object property', () => {
     test('object property', () => {
         expect(data.name).toBe('javascript test');
-        expect(data.version).not.toBe('1.1.0');
+        expect(data.version).toBe('1.0.0');
     });
+    test('object property', () => {
+        expect(data.version).not.toBe('1.1.0');
+    })
+
+    test('rewrite as ', () => {
+        expect(Object.keys(data)).toContain('name')
+        expect(Object.is(data.name, 'javascript test')).toBe(true)
+    })
 })
