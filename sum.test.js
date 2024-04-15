df4273f672f9c17ca8b209ec7f950ef35de64291
Shaikh Imran <skimran0306@gmail.com>
2024-04-15 14:41:39 +0530

    object test

--- sum.test.js
+++ sum.test.js
@@ -8,3 +8,10 @@ test('sum of 4 + 4', () => {
     expect(4 + 4).toBe(8);
 })
 
+test('object test value', () => {
+    const data = { name: 'Imran' };
+    data['age'] = 24;
+    data['address'] = 'bandra east'
+
+    expect(data).toEqual({ name: 'Imran', age: 24, address: 'bandra east' });
+})
